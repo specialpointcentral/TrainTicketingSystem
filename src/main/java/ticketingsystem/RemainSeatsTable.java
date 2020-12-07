864a240808d0fd2d6d9ae7029d12a015e8387268
Jack Hu <spcreply@126.com>
2020-12-07 21:39:56 +0800

    WIP: Fix linearization error

--- src/main/java/ticketingsystem/RemainSeatsTable.java
+++ src/main/java/ticketingsystem/RemainSeatsTable.java
@@ -5,35 +5,43 @@ import java.util.concurrent.atomic.AtomicStampedReference;
 
 public class RemainSeatsTable {
     private int stationNum;
-    private AtomicInteger[][][] remainSeats;
+    private int[][][] remainSeats;
     private AtomicStampedReference<Integer> tag;
 
     public RemainSeatsTable(final int seatnum, final int stationnum) {
         this.stationNum = stationnum;
-        this.tag = new AtomicStampedReference<>(0,0);
-        this.remainSeats = new AtomicInteger[2][][];
+        this.tag = new AtomicStampedReference<>(0, 0);
+        this.remainSeats = new int[2][][];
         for (int tags = 0; tags < 2; ++tags) {
-            this.remainSeats[tags] = new AtomicInteger[stationnum][];
+            this.remainSeats[tags] = new int[stationnum][];
             for (int i = 0; i < stationnum; ++i) {
                 // remainSeats[from][to]
                 // NOTE: 'from' is real, but 'to' is (to+from)
                 // if 'to' is 0, result is always 0
                 // Example: remainSeats[1][2] = (1)->(3)
-                this.remainSeats[tags][i] = new AtomicInteger[stationnum - i];
-                this.remainSeats[tags][i][0] = new AtomicInteger(0);
+                this.remainSeats[tags][i] = new int[stationnum - i];
+                this.remainSeats[tags][i][0] = 0;
                 for (int j = 1; j < stationnum - i; ++j) {
-                    remainSeats[tags][i][j] = new AtomicInteger(seatnum);
+                    remainSeats[tags][i][j] = seatnum;
                 }
             }
         }
     }
 
     public int getRemainSeats(final int departure, final int arrival) {
-        return remainSeats[tag.get() % 2][departure][arrival - departure].get();
+        int currTimestap = tag.getStamp();
+        int currTag = tag.getReference();
+        int remain = remainSeats[currTag % 2][departure][arrival - departure];
+        while (!tag.compareAndSet(currTag, currTag, currTimestap, currTimestap)) {
+            currTimestap = tag.getStamp();
+            currTag = tag.getReference();
+            remain = remainSeats[currTag % 2][departure][arrival - departure];
+        }
+        return remain;
     }
 
-    public void decrementRemainSeats(final int departure, final int arrival) {
-        int currTag = (tag.get() + 1) % 2;
+    public synchronized void decrementRemainSeats(final int departure, final int arrival) {
+        int currTag = (tag.getReference() + 1) % 2;
         // 0 1 2 3 4 5
         // Example: 2->4
         // 0-3 -> 3-5
@@ -41,23 +49,23 @@ public class RemainSeatsTable {
         for (int i = 0; i < arrival; ++i) {
             // arrival station
             for (int j = Math.max(departure, i) + 1; j < stationNum; ++j) {
-                remainSeats[currTag][i][j - i].getAndDecrement();
+                remainSeats[currTag][i][j - i] = remainSeats[(currTag + 1) % 2][i][j - i] - 1;
             }
         }
         // finish modify
-        tag.getAndIncrement();
+        tag.set(currTag, tag.getStamp() + 1);
     }
 
-    public void incrementRemainSeats(final int departure, final int arrival) {
-        int currTag = (tag.get() + 1) % 2;
+    public synchronized void incrementRemainSeats(final int departure, final int arrival) {
+        int currTag = (tag.getReference() + 1) % 2;
         // departure station
         for (int i = 0; i < arrival; ++i) {
             // arrival station
             for (int j = Math.max(departure, i) + 1; j < stationNum; ++j) {
-                remainSeats[currTag][i][j - i].getAndIncrement();
+                remainSeats[currTag][i][j - i] = remainSeats[(currTag + 1) % 2][i][j - i] + 1;
             }
         }
         // finish modify
-        tag.getAndIncrement();
+        tag.set(currTag, tag.getStamp() + 1);
     }
 }
